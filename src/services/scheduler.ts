225821b45ad1339acaa218635ffa635bb467de12
Aleksey Chebykin <aleksey.avatarov@gmail.com>
2025-06-28 14:51:05 +0300

    Second commit

--- src/services/scheduler.ts
+++ src/services/scheduler.ts
@@ -4,14 +4,14 @@ import { updateSpreadsheets } from './google-sheets';
 
 export function startTariffScheduler() {
   console.log('Запускается планировщик тарифов...');
-  cron.schedule('* * * * *', () => {
+  cron.schedule('0 * * * *', () => {
     console.log('[Scheduler] Запуск обновления WB тарифов');
     fetchAndSaveTariffs();
   });
 }
 
 export function startSheetsScheduler() {
-  cron.schedule('* * * * *', () => {
+  cron.schedule('30 * * * *', () => {
     console.log('[Scheduler] Запуск обновления Google Sheets');
     updateSpreadsheets();
   });
